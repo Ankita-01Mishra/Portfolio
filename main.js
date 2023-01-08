00f7aabd08c4afc56f0069beff0ad9e2e7890333
Ankita-01Mishra <boycaptain789@gmail.com>
2023-01-08 19:09:49 +0530

    committed!!

---
+++ main.js
@@ -0,0 +1,7 @@
+document.getElementById('threeLine').addEventListener("click",()=>{
+    document.getElementById('media-menu').style.display="block";
+})
+
+document.getElementById('media-menu').addEventListener('click',()=>{
+    document.getElementById('media-menu').style.display="none";
+})
